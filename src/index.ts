202f7f54f10076bba0818cf3fca9f9a3f9b3d31a
Yasin Silavi <ys.silavi@gmail.com>
2024-02-11 14:22:32 +0330

    feat: add cors and express.json middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.listen(process.env.PORT, () => {
   console.log(`[${process.env.PORT}] Server is running ... 🚀`);
