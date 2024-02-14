f6e0bef3d24737d4c3a21d14a74a05f828b2624a
Yasin Silavi <ys.silavi@gmail.com>
2024-02-14 13:38:29 +0330

    feat: add mongoose schema for user

---
+++ src/models/User.ts
@@ -0,0 +1,48 @@
+import { UUID } from "crypto";
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface UserDocument extends Document {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  verified: boolean;
+  verificationToken: UUID;
+}
+
+const userSchema = new Schema<UserDocument>({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
+});
+
+userSchema.set("timestamps", true);
+
+// userSchema.set('toJSON', {
+//   transform: (_, ret) => {
+//     const { password, verificationToken, __v, ...rest } = ret;
+
+//     return rest;
+//   },
+// });
+
+export default mongoose.model<UserDocument>("User", userSchema);
