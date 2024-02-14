bfd0c58012a4eddcea79965b1d0d589ccfab94fd
Yasin Silavi <ys.silavi@gmail.com>
2024-02-14 13:41:33 +0330

    feat: add users routes middleware

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import cors from "cors";
 import express from "express";
 import { connectDB } from "@/database";
 
+import userRoutes from "@/routes/UserRoutes";
+
 const app = express();
 
 connectDB();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+
+app.use("/api_v1.0/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`[${process.env.PORT}] Server is running ... 🚀`);
