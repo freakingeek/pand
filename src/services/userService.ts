2aac8df53543e2c0006a5886d754e85a57d43a41
Yasin Silavi <ys.silavi@gmail.com>
2024-02-14 15:06:38 +0330

    feat: add ability to create new user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,21 @@
+import { randomUUID } from "crypto";
+import { sendVerificationEmail } from "@/emails";
 import User, { type UserDocument } from "@/models/User";
 
 export async function getAllUsers(): Promise<UserDocument[]> {
   return User.find();
+}
+
+export async function createNewUser(userData: Partial<UserDocument>): Promise<UserDocument> {
+  const hashedPassword = userData.password ? await Bun.password.hash(userData.password) : undefined;
+  const verificationToken = randomUUID();
+
+  userData.password = hashedPassword;
+  userData.verificationToken = verificationToken;
+
+  const user = await User.create(userData);
+
+  await sendVerificationEmail(user.email, user.verificationToken);
+
+  return user;
 }
