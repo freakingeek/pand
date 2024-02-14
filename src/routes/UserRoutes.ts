27ab5743babb74cacfd3bc3d98880bf2b4529db5
Yasin Silavi <ys.silavi@gmail.com>
2024-02-14 15:07:32 +0330

    feat: add create user route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,5 +4,11 @@ import * as userController from "@/controllers/userController";
 const router = express.Router();
 
 router.get("/", userController.getAllUsers);
+router.post("/", userController.createNewUser);
+// router.post('/', userController.createUser);
+// router.delete('/', userController.createUser);
+// router.get('/:id', userController.getUserById);
+
+// router.get('/verify/:token', userController.verifyUser);
 
 export default router;
