e811fd23aa19abd5d15360d2146250997cd728d9
Yasin Silavi <ys.silavi@gmail.com>
2024-02-17 19:30:26 +0330

    fix: change jwt secret key

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import * as response from "@/utils/response";
 import { Request, Response, NextFunction } from "express";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -10,7 +10,7 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
   }
 
   try {
-    const decode = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decode = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET as string);
     (req as any).userId = (decode as any).userId;
 
     next();
